e8962bbf8375273888784b8f1e6df39bc4ddc7bd
yone920 <yone920@gmail.com>
2020-03-04 14:12:56 -0500

    tailwing css breakpont variable name changed

--- config/tailwind.js
+++ config/tailwind.js
@@ -17,12 +17,19 @@ module.exports = {
     backgroundColor: variables.colors,
     borderColor: variables.colors,
 		fontFamily: variables.fonts,
+		// screens: {
+		// 	'only-small': {'max': variables.screens.small}, //apply only to mobile
+		// 	'small': {'min': variables.screens.small}, //screen-small
+		// 	'medium': {'min': variables.screens.medium}, //screen-medium
+		// 	'large': {'min': variables.screens.large}, //screen-large
+		// 	'xlarge': {'min': variables.screens.xlarge} //screen-xtra large
+		// },
 		screens: {
-			'only-small': {'max': variables.screens.small}, //apply only to mobile
-			'small': {'min': variables.screens.small}, //screen-small
-			'medium': {'min': variables.screens.medium}, //screen-medium
-			'large': {'min': variables.screens.large}, //screen-large
-			'xlarge': {'min': variables.screens.xlarge} //screen-xtra large
+			'mobile-only': {'max': variables.screens['small']}, //apply only to mobile
+			'mobile': {'min': variables.screens['small']}, //screen-small
+			'tablet': {'min': variables.screens['medium']}, //screen-medium
+			'desktop': {'min': variables.screens['large']}, //screen-large
+			'desktop-lg': {'min': variables.screens['xlarge']} //screen-xtra large
 		},
   }
 };

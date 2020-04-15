1183e796fa117d59bba910bbfebae37a1fe9505f
yone920 <yone920@gmail.com>
2020-04-15 14:30:56 -0400

    ofcanvas.scss updated

--- src/objects/offcanvas/Offcanvas.js
+++ src/objects/offcanvas/Offcanvas.js
@@ -1,17 +1,17 @@
-'use strict';
+// 'use strict';
 
-class Offcanvas {
-  /**
-   * @param  {object} settings This could be some configuration options.
-   *                           for the pattern module.
-   * @param  {object} data     This could be a set of data that is needed
-   *                           for the pattern module to render.
-   * @constructor
-   */
-  constructor(settings, data) {
-    this.data = data;
-    this.settings = settings;
-  }
-}
+// class Offcanvas {
+//   /**
+//    * @param  {object} settings This could be some configuration options.
+//    *                           for the pattern module.
+//    * @param  {object} data     This could be a set of data that is needed
+//    *                           for the pattern module to render.
+//    * @constructor
+//    */
+//   constructor(settings, data) {
+//     this.data = data;
+//     this.settings = settings;
+//   }
+// }
 
-export default Offcanvas;
+// export default Offcanvas;
